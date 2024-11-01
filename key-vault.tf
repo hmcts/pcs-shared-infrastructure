1280f1476c68b768b8f7fd48896fe7b02fa2d9e7
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2024-11-01 08:34:03 +0000

    Updating Terraform Formatting

--- key-vault.tf
+++ key-vault.tf
@@ -1,15 +1,15 @@
 data "azurerm_client_config" "current" {}
 
 module "key-vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                    = var.product
-  env                        = var.env
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  object_id                  = var.jenkins_AAD_objectId
-  resource_group_name        = azurerm_resource_group.rg.name
-  product_group_name         = var.product_group_name
-  common_tags                = var.common_tags
-  create_managed_identity    = true
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = var.product_group_name
+  common_tags             = var.common_tags
+  create_managed_identity = true
 }
 
 output "vaultName" {
